b849bce02fa0c0c9fb3f7ae3fd1567575a13190a
Richard de Borja <richard.deborja@gmail.com>
2019-07-02 19:54:05 -0400

    Initial Commit

---
+++ ClinTrials/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClinTrials
+//
+//  Created by Richard de Borja on 2019-07-02.
+//  Copyright © 2019 Richard de Borja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
